bf1d3f9264d8bdc319de88a61c3d9e1cdc4654ce
Subham Das <45339522+Subham0209@users.noreply.github.com>
2023-12-17 20:30:05 +0530

    FirstCommiton17thDec

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int d = a+b+c;
+        System.out.println(d);
+    }
+
+
+
+}
